51915875af469e8d42c04cbac18fb49d40183dff
Marla Spencer <marla.spencer@nhm.ac.uk>
2023-01-18 14:19:53 +0000

    Gif

--- Day 3 script.R
+++ Day 3 script.R
@@ -6,3 +6,7 @@ library(tidyverse)
 12+4
 ggplot()+geomplot()
 #I dont know enough code 
+
+#Hello code code code code code
+#I wanted to send you a great gif 
+![flamingo.gif](flamingo.gif) / ![](flamingo.gif)
